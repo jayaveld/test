a451b7b07d61db85fb2bc0a0dbd8e58e63af132c
Adarsh Mammen <adarsh.mammen@gmail.com>
2014-02-25 13:43:01 +0530

    This is the real deal

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-print "This is a test"
-name = raw_input('What is your name?\n')
-print 'Hi, %s.' % name
+prices = {'apple': 0.40, 'banana': 0.50}
+my_purchase = {
+    'apple': 1,
+    'banana': 6}
+grocery_bill = sum(prices[fruit] * my_purchase[fruit]
+                   for fruit in my_purchase)
+print 'I owe the grocer $%.2f' % grocery_bill
--- test2.py
+++ test2.py
@@ -1,5 +1,18 @@
-def greet(name):
-    print 'Hello', name
-greet('Jack')
-greet('Jill')
-greet('Bob')
+from time import localtime
+
+activities = {8: 'Sleeping',
+              9: 'Commuting',
+              17: 'Working',
+              18: 'Commuting',
+              20: 'Eating',
+              22: 'Resting' }
+
+time_now = localtime()
+hour = time_now.tm_hour
+
+for activity_time in sorted(activities.keys()):
+    if hour < activity_time:
+        print activities[activity_time]
+        break
+else:
+    print 'Unknown, AFK or sleeping!'
